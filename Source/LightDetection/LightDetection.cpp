fd0f9ff83e5f48598bc38ab4950363e7b20f799c
dzar <dzar@emendus.ovh>
2016-11-02 13:51:22 +0100

    Repaired project after name change

--- Source/LightDetection/LightDetection.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "LightDetection.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LightDetection, "LightDetection" );
