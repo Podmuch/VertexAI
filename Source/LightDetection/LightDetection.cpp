39988df3785408d899e0d19e9eb5f0614c27e8db
dzar <dzar@emendus.ovh>
2016-11-02 13:22:43 +0100

    Added light detection component

---
+++ Source/LightDetection/LightDetection.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LightDetection.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LightDetection, "LightDetection" );
